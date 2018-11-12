1ef83b09cc06838b4782d7f6c3004154b7fe307b
huanqing_deng <huanqing_deng@sui.com>
2018-11-12 12:50:46 +0800

    Hystrix Dashboard：http://127.0.0.1:8083/hystrix.stream

--- cli-order-service/src/main/java/com/poppy/order/CliOrderServiceApplication.java
+++ cli-order-service/src/main/java/com/poppy/order/CliOrderServiceApplication.java
@@ -2,17 +2,22 @@ package com.poppy.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableFeignClients
 public class CliOrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CliOrderServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CliOrderServiceApplication.class, args);
+    }
 }
+
