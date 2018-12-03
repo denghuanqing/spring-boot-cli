f19ab7ddb0abc4ef1021f7771657e683e3e6ee2f
denghuan1996@163.com <dd17671788208>
2018-12-03 15:01:38 +0800

    add spring cloud kafka

--- cli-order-service/src/main/java/com/poppy/order/controller/OrderController.java
+++ cli-order-service/src/main/java/com/poppy/order/controller/OrderController.java
@@ -2,6 +2,9 @@ package com.poppy.order.controller;
 
 import com.poppy.order.domain.Order;
 import com.poppy.order.service.OrderService;
+import com.poppy.order.stream.producer.KafkaSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class OrderController {
+    private final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    KafkaSender sender;
+
     @RequestMapping("/order/{order_id}")
     public Order get(@PathVariable("order_id") String orderId) {
         return orderService.findById(orderId);
+    }
+
+    @RequestMapping("/order")
+    public void add() {
+        logger.info("send msg [{}]", "order create success!");
+        sender.sendMessage("order create success!");
     }
 }
