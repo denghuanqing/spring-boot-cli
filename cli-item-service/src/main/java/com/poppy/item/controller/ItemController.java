b99ffc139559384bd7b9735177cc3b4429f6931e
DENGHUANQING1 <Poppy2018>
2020-05-11 19:52:36 +0800

    统一管理spring boot & spring cloud 依赖

--- cli-item-service/src/main/java/com/poppy/item/controller/ItemController.java
+++ cli-item-service/src/main/java/com/poppy/item/controller/ItemController.java
@@ -2,6 +2,8 @@ package com.poppy.item.controller;
 
 import com.poppy.item.domain.Item;
 import com.poppy.item.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ItemController {
+
+    Logger logger = LoggerFactory.getLogger(ItemController.class);
+
     @Autowired
     ItemService itemService;
 
     @RequestMapping("/item/{item_id}")
     public Item get(@PathVariable("item_id") long itemId) {
-
+        logger.info("query Item Servie:{}",itemId);
         return itemService.findById(itemId);
     }
 }
